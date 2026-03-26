04219e514f7936c9c3e05bd079b90de50a56e280
Nguyễn Hữu Trung Tín <test123@gmail.com>
2026-03-26 09:21:20 +0700

    fix vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable /tmp/flutter
@@ -6,7 +6,7 @@ export PATH="$PATH:/tmp/flutter/bin"
 
 flutter pub get
 flutter build web --release \
-  --dart-define=RECAPTCHA_SITE_KEY="$RECAPTCHA_SITE_KEY" \
-  --dart-define=API_BASE_URL="$API_BASE_URL" \
-  --dart-define=APP_ENV="$APP_ENV" \
-  --dart-define=APP_ENV_LABEL="$APP_ENV_LABEL"
+  --dart-define=RECAPTCHA_SITE_KEY=$RECAPTCHA_SITE_KEY \
+  --dart-define=API_BASE_URL=$API_BASE_URL \
+  --dart-define=APP_ENV=$APP_ENV \
+  --dart-define=APP_ENV_LABEL=$APP_ENV_LABEL
